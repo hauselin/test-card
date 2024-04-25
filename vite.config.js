4d13486e1454183d2a400081cef4d8d21f8968d2
Hause Lin <hauselin@gmail.com>
2024-04-24 21:21:43 -0400

    First working version

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ export default defineConfig({
 	server: {
 		fs: {
 			allow: [
-				// search up for workspace root
-				// searchForWorkspaceRoot(process.cwd()),
-				// your custom rules
 				'./tmp/',
 				'./tmp',
 				'/tmp',
